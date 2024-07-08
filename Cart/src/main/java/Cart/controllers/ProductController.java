637945ff3f04e6ef90861a009a6d4da93dc1ca2b
saideepak <dogiparthi.sai@northeastern.edu>
2024-07-08 01:23:13 -0400

    Testing Workflow

--- Cart/src/main/java/Cart/controllers/ProductController.java
+++ Cart/src/main/java/Cart/controllers/ProductController.java
@@ -16,13 +16,17 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
-
+    
+    
+    // Add item to cart
     @PostMapping("/add")
     public ResponseEntity<ProductModel> addItem(@Valid @RequestBody ProductModel item) {
         ProductModel createdItem = productService.addItem(item);
         return new ResponseEntity<>(createdItem, HttpStatus.CREATED);
     }
-
+    
+    
+    // Fetching all items from cart
     @GetMapping("/items")
     public ResponseEntity<List<ProductModel>> getAllItems() {
     	List<ProductModel> productList = productService.getAllItems();
